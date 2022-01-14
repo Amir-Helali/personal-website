cec956a9018d07b10d4e4cbd11c6308587abd84d
Amir Helali <54836705+Amir-Helali@users.noreply.github.com>
2022-01-14 04:54:27 -0500

    Navbar Done

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+  <nav className="navbar navbar-expand-lg navbar-dark py-3">
+    <div className='container'>
+    <a className="navbar-brand" href="#">AMIRREZA HELALI</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Me</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Education</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Skills</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact Me</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+</nav>
+    )
+}
+
+export default Navbar;
